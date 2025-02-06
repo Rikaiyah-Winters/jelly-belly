7767ece035336503defacd13aa6f6c247d90b85e
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2025-02-06 10:10:04 -0600

    Imported modules, made api request, and converted json file data

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
+import json
 
 # Initialize the Flask application
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-  return render_template("jellybeans.html")
+@app.route("/", methods=["GET"])
+def jelly_belly_info():
+  response = requests.get("https://jelly-belly-wiki.netlify.app/api/beans?groupName=Jelly%20Belly%20Official%20Flavors")
+  respnonse_data = response.json()
+  jelly_beans = json.loads(respnonse_data)
+  print(jelly_beans)
+  return render_template("/jellybeans.html")
 
 # Run the flask server
 if __name__ == "__main__":
