776913bb15bc1e38fa66284f9a02a8cc814d380d
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2025-02-06 12:14:08 -0600

    Added error list

--- app.py
+++ app.py
@@ -5,14 +5,23 @@ import json
 # Initialize the Flask application
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def jelly_belly_info():
-  response = requests.get("https://jelly-belly-wiki.netlify.app/api/beans?groupName=Jelly%20Belly%20Official%20Flavors")
-  respnonse_data = response.json()
-  jelly_beans = json.loads(respnonse_data)
-  print(jelly_beans)
-  return render_template("/jellybeans.html")
+  errors = []
+  if request.method == "POST":
+    bean = request.form.get("bean")
+    if not bean:
+       errors.append("Oops! Please choose a bean!")
+    if bean:
+         response = requests.get("https://jelly-belly-wiki.netlify.app/api/beans?groupName=Jelly%20Belly%20Official%20Flavors")
+         respnonse_data = response.json()
+         jelly_beans = json.loads(respnonse_data)
+         print(jelly_beans)
+
+
+  return render_template("jellybeans.html", data = jelly_beans)
 
 # Run the flask server
 if __name__ == "__main__":
     app.run()
+
