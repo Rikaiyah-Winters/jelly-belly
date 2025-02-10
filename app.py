58c676b6a8b2760f94c497f1cbb37bd0b6aeaefb
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2025-02-09 20:11:19 -0600

    Figured out how to access flavorNames

--- app.py
+++ app.py
@@ -7,18 +7,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def jelly_belly_info():
-  try:
-    response = requests.get("https://jellybellywikiapi.onrender.com/api/beans/1")
-    response.raise_for_status()  # Check if the request was successful
-    response_data = response.json()
-    jelly_beans = json.dumps(response_data, indent=2)  # Pretty print the JSON response
-  except requests.exceptions.HTTPError as http_err:
-    print(f"HTTP error occurred: {http_err}")
-  #except requests.exceptions.RequestException as err:
-    #print(f"Other error occurred: {err}")
-  #except json.JSONDecodeError as json_err:
-    #print(f"JSON decode error: {json_err}")
-  return render_template("jellybeans.html", data = jelly_beans)
+  response = requests.get("https://jellybellywikiapi.onrender.com/api/Beans")
+  response.raise_for_status()  # Check if the request was successful
+  response_data = response.json() #jsonifies the data
+  beans_info = response_data["items"] 
+  return render_template("jellybeans.html", beans = beans_info)
   
 
 # Run the flask server
