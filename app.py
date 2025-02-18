e42457286d4becb7eabddd8f25d9adadb3dd4da9
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2025-02-18 09:15:35 -0600

    Conected html select options to jelly_belly_info funtion

--- app.py
+++ app.py
@@ -5,15 +5,18 @@ from bean_flavors import prettify_bean_flavor
 # Initialize the Flask application
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def jelly_belly_info():
-  #errors = []
-  response = requests.get("https://jellybellywikiapi.onrender.com/api/Beans")
-  #response.raise_for_status()
-  response_data = response.json()
-  beans_info = response_data["items"]
-  bean_7up = next((bean for bean in beans_info if bean['flavorName'] == '7Up'), None)
-  return render_template("jellybeans.html", bean=bean_7up)
+  if request.method == "POST":
+    chosen_bean = request.form.get("bean-flavor")
+    if chosen_bean:
+      response = requests.get("https://jellybellywikiapi.onrender.com/api/Beans")
+      #response.raise_for_status()
+      response_data = response.json()
+      beans_info = response_data["items"]
+      chosen_bean_info = next((bean for bean in beans_info if bean['beanId'] == str(chosen_bean)), None)
+      return render_template("jellybeans.html", bean=chosen_bean_info)
+  return render_template("jellybeans.html", bean=[])
   
       
 
