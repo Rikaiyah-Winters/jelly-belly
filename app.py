c3479b1e1edf81128c2a1a0202df607e5302fd8b
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2025-02-19 19:52:12 -0600

    Fixed the empty option value error

--- app.py
+++ app.py
@@ -7,23 +7,28 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def jelly_belly_info():
+  errors=[]
   if request.method == "POST":
     chosen_bean = request.form.get("bean-flavor")
-    if int(chosen_bean) < 11:
+    if not chosen_bean:
+      errors.append("Please choose a Jelly Bean Flavor")
+      return render_template("jellybeans.html", bean=[], errors=errors)
+    chosen_bean = int(chosen_bean)
+    if chosen_bean < 11:
       response = requests.get("https://jellybellywikiapi.onrender.com/api/Beans?pageIndex=1")
       #response.raise_for_status()
       response_data = response.json()
       beans_info = response_data["items"]
       chosen_bean_info = next((bean for bean in beans_info if bean['beanId'] == int(chosen_bean)), None)
       return render_template("jellybeans.html", bean=chosen_bean_info)
-    if int(chosen_bean) < 21:
+    if chosen_bean < 21:
       response = requests.get("https://jellybellywikiapi.onrender.com/api/Beans?pageIndex=2")
       #response.raise_for_status()
       response_data = response.json()
       beans_info = response_data["items"]
       chosen_bean_info = next((bean for bean in beans_info if bean['beanId'] == int(chosen_bean)), None)
-      return render_template("jellybeans.html", bean=chosen_bean_info)
-  return render_template("jellybeans.html", bean=[])
+      return render_template("jellybeans.html", bean=chosen_bean_info, errors=[])
+  return render_template("jellybeans.html", bean=[], errors=errors)
       
 
 # Run the flask server
